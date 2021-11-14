dcca81076e2523bed2f282f8ab108a1798c8db4e
Caio Graça Melo <49076270+caiomelo22@users.noreply.github.com>
2021-11-13 22:57:02 -0300

    Definição da classe jogador

--- trab2/myapp/models.py
+++ trab2/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+class Player(models.Model):
+  id = models.AutoField(primary_key=True),
+  name = models.CharField(
+  max_length=100, help_text="Enter player's name")
+  team = models.CharField(
+  max_length=100, help_text="Enter player's team")
+  nation = models.CharField(
+  max_length=100, help_text="Enter player's nation")
+  def __str__(self):
+    return self.name
 
 # Create your models here.
